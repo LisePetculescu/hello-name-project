808dde6ebf65ea086dbc0f1768f462be0ea4e029
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-01-31 13:49:07 +0100

    donso

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-document.body.innerHTML += "Hello JavaScript!";
+//document.body.innerHTML += "Hello JavaScript!";
 console.log("app.js virker");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
