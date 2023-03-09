586ab8a140c3498f04dd0830f82fd5b57c4ed7f6
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-09 11:27:19 +0100

    9/3/23

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+"use strict";
 //document.body.innerHTML += "Hello JavaScript!";
 console.log("app.js virker");
 
-function sayHello() {
+// function sayHello() {
+//   document.querySelector("p").textContent =
+//     "Hej " + document.querySelector("input").value;
+// }
+
+// document.querySelector("button").addEventListener("click", sayHello);
+
+function lol() {
+  let theName = document.querySelector("#fullName").value;
+  console.log(theName);
+  let age = document.querySelector("#alder").value;
+  let email = document.querySelector("#mail").value;
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " + theName + " som er " + age + " år gammel med email " + email;
 }
 
-document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("button").addEventListener("click", lol);
