e1ed40bcc490367c3b985091778b7d70f94b060a
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-09 11:55:09 +0100

    lol

--- app.js
+++ app.js
@@ -19,3 +19,56 @@ function lol() {
 }
 
 document.querySelector("button").addEventListener("click", lol);
+
+console.log(Number("   123  "));
+console.log(Number("123z"));
+console.log(Number(true));
+console.log(Number(false));
+
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+
+// datatyper
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+console.log(bool);
+console.log(num);
+console.log(str);
+console.log(obj);
+console.log(nothing);
+console.log(undf);
+console.log(symbol);
+
+// strings
+let single = "single-quoted";
+let double = "double-qouted";
+let backtics = `backtics`;
+
+console.log(single);
+console.log(double);
+console.log(backtics);
+
+let string = "1234";
+console.log(string);
+
+let number = 1234;
+console.log(number);
+
+// concatenation
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+console.log(n1 + n2);
+console.log(s1 + s2);
+console.log(n1 + s2);
+console.log(s1 + n2);
